d1db7c70cb325212e388b9a7b7a57b092b1b1c1b
yezi <08204407@cumt.edu.cn>
2025-05-06 20:07:16 +0800

    创建空洞文件(空洞部分不占据实际的存储空间)

---
+++ 3_chapter/2_test.c
@@ -0,0 +1,46 @@
+// 创建空洞文件，可用于多线程同时写入数据
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static unsigned char buf[4096];
+
+int main()
+{
+    int fd;
+    int ret;
+
+    fd = open("./test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0744);
+    if (fd == -1)
+    {
+        perror("open error");
+        return -1;
+    }
+
+    lseek(fd, 4096, SEEK_SET);
+    if (ret == -1)
+    {
+        perror("lseek error");
+        close(fd);
+        return -1;
+    }
+
+    ret = write(fd, buf, 4096);
+    if (ret == -1)
+    {
+        perror("write error");
+        close(fd);
+        return -1;
+    }
+
+    printf("write %d bytes\n", ret);
+
+    close(fd);
+
+    return 0;
+}
